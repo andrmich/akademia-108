fd8e918f4879125ac8a1d6428967694da0f7d8ce
aisim <github@mismail.xyz>
2019-03-04 15:12:58 +0100

    all after workshop

---
+++ js/popup.js
@@ -0,0 +1,10 @@
+const showPopup = (event)=> {
+    event.preventDefault();
+    document.getElementById("popup").style.display = "block";
+
+}
+const hidePopup = (event)=> {
+    event.preventDefault();
+    document.getElementById("popup").style.display = "none";
+
+}
